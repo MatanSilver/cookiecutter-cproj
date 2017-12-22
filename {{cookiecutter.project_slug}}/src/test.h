e1d92890589e3b3a580a89aa7ee523c167fd5ca2
MatanSilver <matansilver@gmail.com>
2017-12-22 00:11:10 -0500

    possibly fixed some memory leakage due to an unnecessary copy-on-pass

--- {{cookiecutter.project_slug}}/src/test.h
+++ {{cookiecutter.project_slug}}/src/test.h
@@ -33,9 +33,13 @@ assert_err_t *assert_err_new(const char *name, const char *msg) {
 	return assert_err;
 }
 
-int assert_err_list_append(assert_err_list_t *assert_err_list, assert_err_t assert_err) {
+//int assert_err_list_append(assert_err_list_t *assert_err_list, assert_err_t assert_err) {
+int assert_err_list_append(assert_err_list_t *assert_err_list, const char *name, const char *msg) {
 	if (assert_err_list->err_size < TEST_MAX_ERR) {
-		assert_err_list->assert_errs[assert_err_list->err_size++] = assert_err;
+		assert_err_list->assert_errs[assert_err_list->err_size].name = malloc(sizeof(char)*TEST_STR_SIZE);
+		assert_err_list->assert_errs[assert_err_list->err_size].msg = malloc(sizeof(char)*TEST_STR_SIZE);
+		strcpy(assert_err_list->assert_errs[assert_err_list->err_size].name, name);
+		strcpy(assert_err_list->assert_errs[assert_err_list->err_size++].msg, msg);
 		return 1;
 	} else {
 		return 0;
@@ -65,14 +69,15 @@ void assert_err_list_dump(assert_err_list_t *assert_err_list) {
 
 void assert_expr(assert_err_list_t *assert_err_list, bool expr, const char *name, const char *msg) {
 	assert_err_list->assertions++;
-	if (!expr) {
-		assert_err_t *assert_err = assert_err_new(name, msg);
-		int success = assert_err_list_append(assert_err_list, *assert_err);
+	if (!expr) { //if test fails
+		//assert_err_t *assert_err = assert_err_new(name, msg);
+		//int success = assert_err_list_append(assert_err_list, *assert_err);
+		int success = assert_err_list_append(assert_err_list, name, msg);
 		if (success) {
 			return;
 		} else {
 			assert_err_list->assertions--;
-			assert_err_destroy(assert_err);
+			//assert_err_destroy(assert_err);
 			printf("Error overflow... dumping failures:\n");
 			assert_err_list_dump(assert_err_list);
 			assert_err_list_destroy(assert_err_list);
